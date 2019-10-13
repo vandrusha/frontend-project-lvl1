e693ad629286e7c641bc2a86f664981b9f5e5c9c
vandrusha <a.a.metelsky@gmail.com>
2019-10-13 16:41:07 +0300

    brain-calc added

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-import readLine, { getRandomInt } from '..';
+import readLine, { getRandomInt, game } from '..';
 
 console.log('Welcome to the Brain Games \nAnswer "yes" if the number is even, otherwise answer "no".');
 
@@ -7,20 +7,7 @@ const name = readLine('May I have your name? ');
 
 console.log(`Hi! ${name}`);
 
-for (let i = 0; i <= 2; i += 1) {
-  const randomNumber = getRandomInt();
-  console.log(`Question: ${randomNumber}`);
-  const answer = readLine('Your answer: ');
-  if ((randomNumber % 2 === 0 && answer.toLowerCase() === 'yes') || (randomNumber % 2 !== 0 && answer.toLowerCase() === 'no')) {
-    console.log('Correct!');
-    if (i === 2) {
-      console.log(`Congratulations, ${name}!`);
-    }
-  } else if (randomNumber % 2 === 0 && answer.toLowerCase() !== 'yes') {
-    console.log(`${answer} is wrong answer ;(. Correct answer was 'yes'.\nLet's try again ${name}!`);
-    i = 2;
-  } else if (randomNumber % 2 !== 0 && answer.toLowerCase() !== 'no') {
-    console.log(`${answer} is wrong answer ;(. Correct answer was 'no'.\nLet's try again ${name}!`); 
-    i = 2;
-  }
-}
+const gameExpression = () => getRandomInt();
+const correctAnswer = (expression) => (expression % 2 === 0 ? 'yes' : 'no');
+
+game(name, gameExpression, correctAnswer);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import readlineSync from 'readline-sync';
 
-const readLine = (greeting) => readlineSync.question(greeting);
+const readLine = (input) => readlineSync.question(input);
 
-// get random number between 0 and 100
-const getRandomInt = () => Math.floor(Math.random() * Math.floor(100));
+// get random number between 0 and max or 100 by default
+const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max || 100));
+
+// define cycles - a number of the available correct answers before game ends
+const rounds = 3;
+
+// get random operation
+const randomOperation = () => {
+  const operations = '+-*';
+  return operations[getRandomInt(2)];
+};
+
+// game engine consists with 3 rounds if answer is correct. If answer is incorrect game ends
+// conditions for answer defined in executable files
+const game = (name, gameExpression, correctAnswer) => {
+  for (let i = 1; i <= rounds; i += 1) {
+    const expression = gameExpression();
+    const correct = correctAnswer(expression);
+    console.log(`Question: ${expression}`);
+    const answer = readLine('Your answer: ');
+    if (answer === correct) {
+      console.log('Correct!');
+      if (i === 3) {
+        console.log(`Congratulations, ${name}!`);
+      }
+    } else if (answer !== correct) {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correct}".\nLet's try again ${name}!`);
+      break;
+    }
+  }
+};
 
 export default readLine;
-export { getRandomInt };
+export { getRandomInt, game, randomOperation };
