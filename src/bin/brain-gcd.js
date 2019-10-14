ea04da6eb1d220973dbaaa4b94fc7e8537c5f4f7
vandrusha <a.a.metelsky@gmail.com>
2019-10-14 22:45:14 +0300

    brain-gcd game added

---
+++ src/bin/brain-gcd.js
@@ -0,0 +1,40 @@
+#! /usr/bin/env node
+import { cons, car, cdr } from 'hexlet-pairs';
+import readLine, { getRandomInt, game } from '..';
+
+console.log('Welcome to the Brain Games \nFind the greatest common divisor of given numbers.');
+
+const name = readLine('May I have your name? ');
+
+console.log(`Hi! ${name}`);
+
+const makePair = () => cons(getRandomInt(), getRandomInt());
+const gameExpression = () => `${car(makePair())} ${cdr(makePair())}`;
+
+const correctAnswer = (expression) => {
+  let firstValue = '';
+  let secondValue = '';
+  for (let i = 0; i < expression.length; i += 1) {
+    if (expression[i] !== ' ') {
+      firstValue += expression[i];
+    } else { break; }
+  }
+  
+  for (let i = firstValue.length + 1; i < expression.length; i += 1) {
+    if (expression[i] !== ' ') {
+      secondValue += expression[i];
+    } else { break; }
+  }
+  
+  const min = Number(firstValue) < Number(secondValue) ? Number(firstValue) : Number(secondValue)
+  let acc = min;
+  for (let i = min; i >= 1; i -= 1) {
+    if (Number(firstValue) % i === 0 && Number(secondValue) % i === 0) {
+      acc = i;
+      break;
+    }
+  }
+  return String(acc);
+};
+
+game(name, gameExpression, correctAnswer);
